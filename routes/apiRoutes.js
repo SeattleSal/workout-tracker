2ef8f26936601d18297937693e2ab833d7590159
Sally Perez <seattlesally1@gmail.com>
2021-02-13 09:06:02 -0800

    added get and post workout api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,5 +4,27 @@ const router = require("express").Router();
 const Workout = require("../models/Workout");
 
 // routes
+// GET workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+});
+
+// POST - create workout
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
 
 module.exports = router;
