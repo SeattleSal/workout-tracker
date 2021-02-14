1631984c70375742072d4d0e5a61aa009e495f97
Sally Perez <seattlesally1@gmail.com>
2021-02-13 20:29:51 -0800

    updated database name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", { useNewUrlParser: true })
 
 // Routes
 app.use(require("./routes/htmlRoutes"));
